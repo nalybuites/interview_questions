81e6da1539a8a5fbc0187f61e45ebb72e365273b
Mike Wright <mwright@appnexus.com>
2015-04-29 21:26:11 -0400

    Moving macros to util. Fixing includes for Linux.

--- src/sorting.c
+++ src/sorting.c
@@ -1,22 +1,13 @@
-//#include <malloc.h>
+#include <malloc.h>
 #include <math.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h> // for osx
 #include <string.h>
+
+#include "util.h"
 
 typedef int compar_t(const void *, const void *);
 typedef void sort_t(int [], unsigned int, compar_t *);
-
-#define max(a,b) \
-	({ __typeof__ (a) _a = (a); \
-		__typeof__ (b) _b = (b); \
-		_a > _b ? _a : _b; })
-
-#define min(a,b) \
-	({ __typeof__ (a) _a = (a); \
-		__typeof__ (b) _b = (b); \
-		_a < _b ? _a : _b; })
 
 static int
 int_comparator(const void *a, const void *b)
