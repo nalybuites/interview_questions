96b3fc7ca6e98df4d3fae3cbf0b0d1cd48316861
Mike L. Wright <nalybuites@gmail.com>
2015-05-11 16:12:56 +0000

    Adding comment

--- src/sum_array.c
+++ src/sum_array.c
@@ -1,5 +1,7 @@
 #include "../include/sum_array.h"
 
+/* Problem 1 from https://blog.svpino.com/2015/05/07/five-programming-problems-every-software-engineer-should-be-able-to-solve-in-less-than-1-hour */
+
 int
 sum_for_loop(int *nums, unsigned int n_nums)
 {
